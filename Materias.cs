a4c963465bd11a83d1d455e02caa48e8107c54d5
AlymiReyna <alymireyna22@gmail.com>
2021-11-23 21:42:34 -0600

    Agregué atributos a la clase Materias

--- Materias.cs
+++ Materias.cs
@@ -1,11 +1,15 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Collections;
 
 namespace EDD
 {
     class Materias
     {
-        
+        //Declaración de los ArrayList
+        ArrayList IDMateria = new ArrayList();
+        ArrayList nombreMateria = new ArrayList();
+ 
     }
 }
