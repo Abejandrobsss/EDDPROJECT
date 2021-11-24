41e6e0030d233032f194ecc5ea523e47fef754db
AlymiReyna <alymireyna22@gmail.com>
2021-11-23 21:41:25 -0600

    Agregué atributos a la clase Alumno

--- Alumno.cs
+++ Alumno.cs
@@ -1,11 +1,19 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Collections;
 
 namespace EDD
 {
     class Alumno
     {
         
+        //Declaración de los ArrayList
+        ArrayList IDAlumno = new ArrayList();
+        ArrayList nombre = new ArrayList();
+        ArrayList apellidoPaterno = new ArrayList();
+        ArrayList apellidoMaterno = new ArrayList();
+        ArrayList fechaNacimiento = new ArrayList();
+        ArrayList semestre = new ArrayList();
     }
 }
