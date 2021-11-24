eadfc12c8a4b4145d6223244bdf1bbd52d8377b1
AlymiReyna <alymireyna22@gmail.com>
2021-11-23 21:43:46 -0600

    Agregué atributos a la clase MateriasPorAlumno

--- MateriasPorAlumno.cs
+++ MateriasPorAlumno.cs
@@ -1,11 +1,17 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Collections;
 
 namespace EDD
 {
     class MateriasPorAlumno
     {
-        
+        //Declaración de los ArrayList
+        ArrayList IDAsoc = new ArrayList();
+        ArrayList IDAlumno = new ArrayList();
+        ArrayList IDMateria = new ArrayList();
+        ArrayList calificacion = new ArrayList();
+
     }
 }
